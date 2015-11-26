33c19fbb1847a9a234ba3b2c9242f9df9fd283da
VijayAllagi <[vijay.allagi@gmail.com]>
2015-11-26 10:58:42 +0530

    Express Commits

--- web.js
+++ web.js
@@ -1,7 +1,18 @@
+//var gzippo = require('gzippo');
+//  var express = require('express');
+//  var app = express();
+// 
+//  app.use(express.logger('dev'));
+//  app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+//  app.listen(process.env.PORT || 5000);
 var gzippo = require('gzippo');
-  var express = require('express');
-  var app = express();
- 
-  app.use(express.logger('dev'));
-  app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-  app.listen(process.env.PORT || 5000);
+var express = require('express');
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+app.listen(PORT, onListen);
+ app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+
+function onListen() {
+  console.log('Listening on', PORT);
+}
