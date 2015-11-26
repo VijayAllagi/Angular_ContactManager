9425d723255bb841c72b642e468e4a294a3b2a39
VijayAllagi <[vijay.allagi@gmail.com]>
2015-11-26 09:56:54 +0530

    Added the test app

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -16,44 +16,44 @@ describe('Controller: indexctrl', function () {
     });
   }));
 
-//  it('contacts length to be 6', function () {
-//    expect(scope.contacts.length).toBe(6);
-//  });
-// it('check the  length to be 0', function () {
-//    scope.contacts =[];
-//    expect(scope.contacts.length).toBe(0);
-//  });
-//
-//it('check the  getContact function', function () {
-//    scope.contacts =[{
-//              id: 1,
-//              name : 'Terrence S. Hatfield',
-//              tel: '651-603-1723',
-//              email: 'TerrenceSHatfield@rhyta.com'
-//            }];
-//     scope.getContact(1);
-//    expect(scope.contact.id).toBe(1);
-//  });
-//    
-//it('check the  delete function', function () {
-//    scope.contacts =[{
-//              id: 4,
-//              name : 'sss',
-//              tel: '651-603-1723',
-//              email: 'dfcf@rhyta.com'
-//            }];
-//     scope.deleteContact(4);
-//    expect(scope.contacts.length).toBe(0);
-//  });
-//it('check the  delete function wrong input', function () {
-//    scope.contacts =[{
-//              id: 4,
-//              name : 'sss',
-//              tel: '651-603-1723',
-//              email: 'dfcf@rhyta.com'
-//            }];
-//     scope.deleteContact(1);
-//    expect(scope.contacts.length).toBe(1);
-//  });
+  it('contacts length to be 6', function () {
+    expect(scope.contacts.length).toBe(6);
+  });
+ it('check the  length to be 0', function () {
+    scope.contacts =[];
+    expect(scope.contacts.length).toBe(0);
+  });
+
+it('check the  getContact function', function () {
+    scope.contacts =[{
+              id: 1,
+              name : 'Terrence S. Hatfield',
+              tel: '651-603-1723',
+              email: 'TerrenceSHatfield@rhyta.com'
+            }];
+     scope.getContact(1);
+    expect(scope.contact.id).toBe(1);
+  });
+    
+it('check the  delete function', function () {
+    scope.contacts =[{
+              id: 4,
+              name : 'sss',
+              tel: '651-603-1723',
+              email: 'dfcf@rhyta.com'
+            }];
+     scope.deleteContact(4);
+    expect(scope.contacts.length).toBe(0);
+  });
+it('check the  delete function wrong input', function () {
+    scope.contacts =[{
+              id: 4,
+              name : 'sss',
+              tel: '651-603-1723',
+              email: 'dfcf@rhyta.com'
+            }];
+     scope.deleteContact(1);
+    expect(scope.contacts.length).toBe(1);
+  });
     
 });
